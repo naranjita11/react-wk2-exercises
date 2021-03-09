c1903733ecaf4b3e34121112c4ee238ab6053eb2
Kate Fox <katefox100@gmail.com>
2021-03-09 14:49:39 +0000

    ajax challenges

--- src/ajax.js
+++ src/ajax.js
@@ -1,3 +1,32 @@
 // import axios with configuration
 import axios from "./axios";
 
+// 1. Use console.log() to output an array with all of your article data from the API:
+
+// axios.get("/articles").then(({ data }) => {
+//     console.log(data.data);
+// });
+
+
+// 2. Create a new article using axios and then console.log() its ID
+
+// axios.post("/articles", {
+//     title: "Field Mice",
+//     content: "They're little and cute and dinky.",
+//     tags: ["mice", "cute"],
+//     }).then(({ data }) => {
+//             console.log(data.data.id);
+//         });
+
+// 3. Fetch one of your articles from the API and console.log() it:
+
+axios.get("/articles/45").then(({ data }) => {
+    console.log(data.data);
+});
+
+// 4. Add a new tag to one of your articles and console.log() the tags array from the response:
+
+// axios.patch("/articles/45", {tags: ...tags, "cute"}).then(({ data }) => {
+//     console.log(data.data.tags);
+// });
+
