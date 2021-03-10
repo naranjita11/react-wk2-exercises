3a6bafc65b08230367d4c1c787a15c740963fcd5
Kate Fox <katefox100@gmail.com>
2021-03-10 16:18:50 +0000

    Routes and Switch set up with FourOhFour

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,22 +1,33 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Articles from "./News/Articles.js";
 import Article from "./News/Article.js";
 import Header from "./News/Header.js";
+import CreateArticle from "./News/CreateArticle.js";
+import FourOhFour from "../FourOhFour";
 // import FourOhFour from "./FourOhFour";
 
 const App = () => (
   <Router>
-    
-    <Header />
-    
-    <Route exact path="/news">
-      <Articles />
-    </Route>
+    <>
+      <Header />
+      <Switch>
+          
+        <Route exact path="/news">
+          <Articles />
+        </Route>
 
-    <Route exact path="/news/:id" render={ ({ match }) => (
-      <Article id={ match.params.id } />
-    ) } />
+        <Route exact path="/news/create">
+          <CreateArticle />
+        </Route>
 
+        <Route exact path="/news/:id" render={ ({ match }) => (
+          <Article id={ match.params.id } />
+        ) } />
+
+        <FourOhFour />
+
+      </Switch>
+    </>     
   </Router>
  
 );
---
+++ src/FourOhFour.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class FourOhFour extends Component {
+    
+    render() {
+        return <p>404: Page not found</p>;
+    }
+    
+}
+
+export default FourOhFour;
